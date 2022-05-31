3da8c7871990779fa2c462b158376e6741aba5f5
916-Andrei-Saramet <andreisaramet21@gmail.com>
2022-05-31 21:57:33 +0300

    jpa repositories

--- lab11/opera/core/src/main/java/ro/ubb/opera/core/repository/Repository.java
+++ lab11/opera/core/src/main/java/ro/ubb/opera/core/repository/Repository.java
@@ -1,21 +1,11 @@
 package ro.ubb.opera.core.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 import ro.ubb.opera.core.model.BaseEntity;
-import ro.ubb.opera.core.model.exceptions.ValidatorException;
 
 import java.io.Serializable;
-import java.util.Optional;
 
 @NoRepositoryBean
-public interface Repository<ID extends Serializable, T extends BaseEntity<ID>> {
-    Optional<T> findOne(ID id);
-
-    Iterable<T> findAll();
-
-    Optional<T> save(T entity) throws ValidatorException;
-
-    Optional<T> delete(ID id);
-
-    Optional<T> update(T entity) throws ValidatorException;
+public interface Repository<ID extends Serializable, T extends BaseEntity<ID>> extends JpaRepository<T, ID> {
 }
