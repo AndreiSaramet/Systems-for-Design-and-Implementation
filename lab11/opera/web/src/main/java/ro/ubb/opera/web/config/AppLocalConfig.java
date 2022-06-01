f07088f28d8712552fdce7d6bf65711f7faec39b
916-Andrei-Saramet <andreisaramet21@gmail.com>
2022-06-01 14:10:03 +0300

    app local config

--- lab11/opera/web/src/main/java/ro/ubb/opera/web/config/AppLocalConfig.java
+++ lab11/opera/web/src/main/java/ro/ubb/opera/web/config/AppLocalConfig.java
@@ -2,9 +2,12 @@ package ro.ubb.opera.web.config;
 
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import ro.ubb.opera.core.config.JPAConfig;
 
 @Configuration
 @ComponentScan({"ro.ubb.opera.core"})
+@Import({JPAConfig.class})
+@PropertySources({@PropertySource(value = "classpath:local/db.properties")})
 public class AppLocalConfig {
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
