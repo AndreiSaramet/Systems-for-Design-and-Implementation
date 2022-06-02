f43c49c038a023823857355b91da18692dc2589f
916-Andrei-Saramet <andreisaramet21@gmail.com>
2022-06-02 12:26:01 +0300

    jpa config update

--- lab11/opera/core/src/main/java/ro/ubb/opera/core/config/JPAConfig.java
+++ lab11/opera/core/src/main/java/ro/ubb/opera/core/config/JPAConfig.java
@@ -2,7 +2,9 @@ package ro.ubb.opera.core.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import lombok.Value;
+
+import org.postgresql.Driver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -17,7 +19,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
-import java.sql.Driver;
+
 
 @Configuration
 @EnableJpaRepositories({"ro.ubb.opera.core.repository"})
