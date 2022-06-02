b90e96c95c295185f59aa1dd2d53e3e720590ac6
916-Andrei-Saramet <andreisaramet21@gmail.com>
2022-06-02 12:24:24 +0300

    composer service update

--- lab11/opera/core/src/main/java/ro/ubb/opera/core/service/ComposerService.java
+++ lab11/opera/core/src/main/java/ro/ubb/opera/core/service/ComposerService.java
@@ -2,10 +2,13 @@ package ro.ubb.opera.core.service;
 
 import ro.ubb.opera.core.model.Composer;
 
+import java.util.List;
 import java.util.Set;
 
 public interface ComposerService extends Service {
     Composer addComposer(String name, String nationality, String musicalPeriod);
+
+    Composer addComposer(Composer composer);
 
     Composer findComposerById(Integer id);
 
@@ -17,7 +20,15 @@ public interface ComposerService extends Service {
 
     Set<Composer> getAllComposers();
 
+    List<Composer> getAllComposersSortedByName(boolean ascending);
+
+    List<Composer> getAllComposersSortedByNationality(boolean ascending);
+
+    List<Composer> getAllComposersSortedByMusicalPeriod(boolean ascending);
+
     Composer updateComposer(Integer id, String name, String nationality, String musicalPeriod);
+
+    Composer updateComposer(Composer composer);
 
     Composer deleteComposer(Integer id);
 }
--- lab11/opera/core/src/main/java/ro/ubb/opera/core/service/ComposerServiceImpl.java
+++ lab11/opera/core/src/main/java/ro/ubb/opera/core/service/ComposerServiceImpl.java
@@ -2,25 +2,26 @@ package ro.ubb.opera.core.service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ro.ubb.opera.core.model.Composer;
 import ro.ubb.opera.core.model.validators.ComposerValidator;
 import ro.ubb.opera.core.model.validators.Validator;
-import ro.ubb.opera.core.repository.Repository;
+import ro.ubb.opera.core.repository.ComposerRepository;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class ComposerServiceImpl implements ComposerService {
     private final Validator<Composer> composerValidator;
 
     @Autowired
-    private final Repository<Integer, Composer> composerRepository;
+    private final ComposerRepository composerRepository;
 
-    public ComposerServiceImpl(Repository<Integer, Composer> composerRepository) {
+    public ComposerServiceImpl(ComposerRepository composerRepository) {
         this.composerValidator = new ComposerValidator();
         this.composerRepository = composerRepository;
     }
@@ -33,28 +34,61 @@ public class ComposerServiceImpl implements ComposerService {
     }
 
     @Override
+    public Composer addComposer(Composer composer) {
+        this.composerValidator.validate(composer);
+        return this.composerRepository.save(composer);
+    }
+
+    @Override
     public Composer findComposerById(Integer id) {
         return this.composerRepository.findById(id).orElse(null);
     }
 
     @Override
     public Composer findComposerByName(String name) {
-        return this.composerRepository.findAll().stream().filter(composer -> composer.getName().equals(name)).findFirst().orElse(null);
+//        return this.composerRepository.findAll().stream().filter(composer -> composer.getName().equals(name)).findFirst().orElse(null);
+        return this.composerRepository.findByName(name).stream().findFirst().orElse(null);
     }
 
     @Override
     public Set<Composer> findComposersByNationality(String nationality) {
-        return this.composerRepository.findAll().stream().filter(composer -> composer.getNationality().equals(nationality)).collect(Collectors.toSet());
+//        return this.composerRepository.findAll().stream().filter(composer -> composer.getNationality().equals(nationality)).collect(Collectors.toSet());
+        return new HashSet<>(this.composerRepository.findByNationality(nationality));
     }
 
     @Override
     public Set<Composer> findComposersByMusicalPeriod(String musicalPeriod) {
-        return this.composerRepository.findAll().stream().filter(composer -> composer.getMusicalPeriod().equals(musicalPeriod)).collect(Collectors.toSet());
+//        return this.composerRepository.findAll().stream().filter(composer -> composer.getMusicalPeriod().equals(musicalPeriod)).collect(Collectors.toSet());
+        return new HashSet<>(this.composerRepository.findByMusicalPeriod(musicalPeriod));
     }
 
     @Override
     public Set<Composer> getAllComposers() {
         return new HashSet<>(this.composerRepository.findAll());
+    }
+
+    @Override
+    public List<Composer> getAllComposersSortedByName(boolean ascending) {
+        if (ascending) {
+            return this.composerRepository.findAll(new Sort(Sort.Direction.ASC, "name"));
+        }
+        return this.composerRepository.findAll(new Sort(Sort.Direction.DESC, "name"));
+    }
+
+    @Override
+    public List<Composer> getAllComposersSortedByNationality(boolean ascending) {
+        if (ascending) {
+            return this.composerRepository.findAll(new Sort(Sort.Direction.ASC, "nationality"));
+        }
+        return this.composerRepository.findAll(new Sort(Sort.Direction.DESC, "nationality"));
+    }
+
+    @Override
+    public List<Composer> getAllComposersSortedByMusicalPeriod(boolean ascending) {
+        if (ascending) {
+            return this.composerRepository.findAll(new Sort(Sort.Direction.ASC, "musicalPeriod"));
+        }
+        return this.composerRepository.findAll(new Sort(Sort.Direction.DESC, "musicalPeriod"));
     }
 
     @Override
@@ -67,6 +101,20 @@ public class ComposerServiceImpl implements ComposerService {
         composer.setName(name);
         composer.setNationality(nationality);
         composer.setMusicalPeriod(musicalPeriod);
+        this.composerValidator.validate(composer);
+        return composer;
+    }
+
+    @Override
+    @Transactional
+    public Composer updateComposer(Composer composer) {
+        Composer updateComposer = this.composerRepository.findById(composer.getId()).orElse(null);
+        if (updateComposer == null) {
+            return null;
+        }
+        updateComposer.setName(composer.getName());
+        updateComposer.setNationality(composer.getNationality());
+        updateComposer.setMusicalPeriod(composer.getMusicalPeriod());
         this.composerValidator.validate(composer);
         return composer;
     }
