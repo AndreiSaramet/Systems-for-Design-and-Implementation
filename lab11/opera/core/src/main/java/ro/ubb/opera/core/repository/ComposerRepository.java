4604f0bbb8a73f7976ce0d6e2389260c83426c0f
916-Andrei-Saramet <andreisaramet21@gmail.com>
2022-06-02 12:26:55 +0300

    composer repository add filtering

--- lab11/opera/core/src/main/java/ro/ubb/opera/core/repository/ComposerRepository.java
+++ lab11/opera/core/src/main/java/ro/ubb/opera/core/repository/ComposerRepository.java
@@ -2,5 +2,12 @@ package ro.ubb.opera.core.repository;
 
 import ro.ubb.opera.core.model.Composer;
 
+import java.util.List;
+
 public interface ComposerRepository extends Repository<Integer, Composer> {
+    List<Composer> findByName(String name);
+
+    List<Composer> findByNationality(String nationality);
+
+    List<Composer> findByMusicalPeriod(String musicalPeriod);
 }
